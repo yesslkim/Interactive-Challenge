f0c4d7d014175d832ba020f666cecfe95d420095
Ye Seul Kim <yskim940914@gmail.com>
2023-01-29 20:28:54 +0900

    Feat: contact 페이지 추가 및 스타일링

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,17 @@
 const Contact = () => {
-  return ( <div>contact</div> );
+  return (
+    <main className='main contact'>
+      <p>
+        인터랙티브한 웹사이트를 만드는 것을 목표로 공부하고 있습니다.
+      </p>
+      <h3>Contact</h3>
+      <ul>
+        <li><a href="https://github.com/yesslkim">깃허브</a></li>
+        <li><a href="https://yesslkim.tistory.com/">블로그</a></li>
+        <li><a href="mailto:yskim940914@gmail.com">이메일<span>(yskim940914@gmail.com)</span></a></li>
+      </ul>
+    </main>
+  );
 }
  
 export default Contact;
