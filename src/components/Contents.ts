48947a64ad985a718e20b1ea9d817b89e1353c93
Ye Seul Kim <yskim940914@gmail.com>
2023-01-29 20:17:20 +0900

    Feat: 컨텐츠 추가

--- src/components/Contents.ts
+++ src/components/Contents.ts
@@ -1,5 +1,6 @@
 import img230125 from '../assets/images/230125.png';
 import img230128 from '../assets/images/230128.png';
+import img230129 from '../assets/images/230129.png';
 
 export const contents = [
   {
@@ -11,18 +12,28 @@ export const contents = [
     pageId: 'dccqwHrTK',
     src: img230128,
     alt: '소녀가 좋아하고 싫어하는 것'
+  },
+  {
+    pageId: '7nfSRpMU_',
+    src: img230129,
+    alt: '강아지와 뼈다귀'
   }
 ]
 
 export const contentsDetail = [
   {
     pageId: 'H5kZsMuJj',
-    title: '움직이는 눈동자',
-    para: '마우스를 움직이면 눈동자가 움직입니다.'
+    title: '230125: 움직이는 눈동자',
+    para: 'p5.js / 마우스를 이동 시, 눈동자가 움직이는 모션'
   },
   {
     pageId: 'dccqwHrTK',
-    title: 'LIKE OR HATE',
-    para: '요소를 드래그하면 소녀의 표정이 변합니다.'
+    title: '230128: LIKE OR HATE',
+    para: 'p5.js / 요소 드래그 시, 소녀의 표정이 변환되는 모션'
+  },
+  {
+    pageId: '7nfSRpMU_',
+    title: '230129: 강아지',
+    para: 'p5.js / 인스턴스를 통한 여러 컴포넌트 생성 및 떨어지는 모셥 구현. 강아지 클릭 시 점프 모션 구현'
   }
 ]
